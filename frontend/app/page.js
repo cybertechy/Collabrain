00c3e6f68ac2a5ee573825dfe37477179f866d9b
Cybertechy <siddhtailor96@gmail.com>
2023-12-03 23:18:45 +0400

    Added Frontend

---
+++ frontend/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-xl font-bold">Hello World</h1>
+      <p>Welcome to collabrain</p>
+    </div>
+  )
+}
