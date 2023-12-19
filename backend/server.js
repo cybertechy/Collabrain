9e8cbe08aef5fbd7acc74f0c608a4a23fb57b538
DarkEmbers <tsharihar@gmail.com>
2023-12-19 13:44:24 +0400

    Basic firestore implementation

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require("./firebase.js");
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -7,15 +8,26 @@ app.use(bodyParser.json());
 const port = 8080;
 
 // Get the Routes
-const AuthRoutes = require("./routes/Authentication.js");
+// const AuthRoutes = require("./routes/Authentication.js");
 
 
 // Use the Routes
-app.use("/api/auth", AuthRoutes);
+// app.use("/api/auth", AuthRoutes);
 
 app.get("/api/home", (req, res) =>
 {
 	res.json({message: "Hello World!"});
+});
+
+app.get("/api/add", (req, res) =>
+{
+	data = {
+		name: "John Doe",
+		email: "johndoe@gmail.com",
+		password: "password",
+	}
+	db.addData("users", data);
+	res.json({message: "Success!"});
 });
 
 app.listen(port, () =>
