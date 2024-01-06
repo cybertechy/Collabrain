680b0b9a7a4364c9734052478125756e3192f2e4
Johan Thomas <jt2026@hw.ac.uk>
2024-01-06 22:06:09 +0400

    first com, front end login add: 3 login pages, assets, 2 components req: functionality for OTP / login

---
+++ frontend/app/otp/page.jsx
@@ -0,0 +1,41 @@
+"use client"
+
+const { isAuth } = require("_firebase/auth"); // Import the authentication functions
+const { useRouter } = require('next/navigation');
+import Button from '../../components/ui/button';
+import InputField from '../../components/ui/input';
+
+
+export default function Register()
+{
+	const router = useRouter();
+
+	if (isAuth())
+		router.push('/dashboard'); // Redirect to dashboard
+
+	return (
+		<div>
+		<div className = "justify-center items-center flex flex-col">
+		<img className = "w-28 fixed top-2" src='.//assets/images/logo_whitebackground.png'/>
+		<div className="bg-secondary flex flex-col justify-center items-center px-16 py-10 rounded-2xl">
+			<p className=" text-black font-poppins mb-2 ">You have received an OTP in your email address!</p>
+			<br />
+            <p className=" text-black font-poppins mb-2 italic">Enter it in the input below!</p>
+			<form onSubmit={()=>{}} style={{ textAlign: "center" }}>
+        <InputField placeholder="OTP" />
+        <br />
+	
+        <Button text="Confirm" color= "primary" onClick={()=>{router.push('/dashboard');}} />
+        <hr className="border-t-1 border-solid border-gray-400 mb-5"></hr>
+        
+      </form>
+        <a className='font-poppins text-primary text-sm italic'>Resend OTP</a>
+	 
+		</div>
+
+		
+		</div>
+
+		</div>
+	)
+}
--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -2,6 +2,12 @@
 
 const { isAuth, emailSignIn, serviceSignIn } = require("_firebase/auth"); // Import the authentication functions
 const { useRouter } = require('next/navigation');
+import Button from '../components/ui/button';
+import InputField from '../components/ui/input';
+import GoogleIcon from '../public/assets/svg/google.svg';
+import MicrosoftIcon from '../public/assets/svg/microsoft.svg';
+import TwitterIcon from '../public/assets/svg/twitterx.svg';
+import FacebookIcon from '../public/assets/svg/facebook.svg';
 
 export default function Home()
 {
@@ -11,23 +17,46 @@ export default function Home()
 		router.push('/dashboard'); // Redirect to dashboard
 
 	return (
-		<div className="flex flex-col justify-center items-center">
-			<h1 className="text-xl font-bold">Hello World</h1>
-			<p>Welcome to collabrain</p>
-			<p>Lets build this collobaration platform</p>
+		<div>
+		<div className = "justify-center items-center flex flex-col">
+		<img className = "w-28 fixed top-2"src='.//assets/images/logo_whitebackground.png'/>
+		<div className="bg-secondary flex flex-col justify-center items-center px-16 py-10 rounded-2xl">
+			<h1 className="text-2xl text-black font-poppins mb-2">Log into Collabrain</h1>
+
 
 			{/* The following code is just an example to show what functions to call for authentication
 				it would be probably better to implement the UI with a library of prebuilt components */}
 
 			<br />
 			<form onSubmit={emailSignIn} style={{ textAlign: "center" }}>
-				<input style={{ margin: 10, padding: 5, color: "black" }} type="email" name="email" /> <br />
-				<input style={{ margin: 10, padding: 5, color: "black" }} type="password" name="password" /> <br />
-				<button style={{ backgroundColor: "white", borderRadius: 5, color: "black", padding: "5px", margin: 10 }} type="submit">Submit</button>
-			</form>
+        <InputField placeholder="Email Address" />
+        <br />
+        <InputField placeholder="Password" />
+        <br />
+		<p className="text-xs text-gray-600 font-poppins text-left"><a>Forgot your password?</a></p>
+        <Button text="Log In" color= "primary" onClick={emailSignIn} />
+		<hr className="border-t-1 border-solid border-gray-400"></hr>
+      </form>
+	 
+	  <span className= "">
+		<button onClick={() => serviceSignIn("google")} className="bg-white py-1 px-1 rounded-full m-5"><GoogleIcon className="h-8 w-8 text-gray-500"></GoogleIcon></button> 
+  <button className="bg-white py-1 px-1 rounded-full m-5"><FacebookIcon className="h-8 w-8 text-gray-500"></FacebookIcon></button>
+  <button onClick={() => serviceSignIn("microsoft")} className="bg-white py-1 px-1 rounded-full m-5"><MicrosoftIcon className="h-8 w-8 text-gray-500"></MicrosoftIcon></button>
+  <button className="bg-white py-1 px-1 rounded-full m-5"><TwitterIcon className="h-8 w-8 text-gray-500"></TwitterIcon></button>
+  </span>
+		</div>
 
-			<button onClick={() => serviceSignIn("microsoft")}>Sign In with microsoft</button>
-			<button onClick={() => serviceSignIn("google")}>Sign In with google</button>
+		
+		</div>
+		<div className="bg-secondary flex flex-col justify-center items-center px-16 py-10 rounded-2xl mt-5">
+			<h1 className="text-2xl text-black font-poppins mb-2">Don't Have An Account?</h1>
+
+
+			
+        <Button text="Register" color= "teritary" onClick={()=>{router.push('/register');}} />
+	
+		</div>
+
 		</div>
 	)
 }
