8e765fa5be377a0e97aed59c56a27eb100de42b5
Johan John Thomas <90568542+johanjohnthomas@users.noreply.github.com>
2024-01-10 23:22:00 +0400

    Login pages (#35)
    * Added kbs endpoint
    
    * first workflow
    
    * modified image
    
    * path chnages
    
    * support main branch
    
    * first com, front end login
    add: 3 login pages, assets, 2 components
    req: functionality for OTP / login
    
    * made changes
    
    * fixes: resolution I was working on was wrong, so the sizing of components were wrong, changed register to be  a grid based layout and now register is shown as a line in login.
    
    * implemented required changes, added background, added color option to input components, added a spinner animation while pages load, additional options for navigation
    
    ---------
    
    Co-authored-by: Cybertechy <siddhtailor96@gmail.com>

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -1,66 +1,99 @@
 "use client";
 
 const { signOut, isAuth, getToken } = require("_firebase/auth"); // Import the authentication functions
-const { useRouter } = require('next/navigation');
-const axios = require('axios');
+const { useRouter } = require("next/navigation");
+const axios = require("axios");
 
-export default function Dashboard()
-{
-	const router = useRouter();
-	if (!isAuth())
-	{
-		// router.push('/'); // Redirect to home page
-		return <h1 className="text-xl font-bold">Please sign in</h1>;
-	}
+export default function Dashboard() {
+    const router = useRouter();
+    if (!isAuth()) {
+        // router.push('/'); // Redirect to home page
+        return <h1 className="text-xl font-bold">Please sign in</h1>;
+    }
 
-	// NOTE: Not finished
-	// Needs to be tested with backend
+    // NOTE: Not finished
+    // Needs to be tested with backend
 
-	let currentDoc;
+    let currentDoc;
 
-	const createDoc = async () =>
-	{
-		// Create a new document
-		const title = document.querySelector("#doc-title").value;
-		const content = document.querySelector("#doc-text").value;
-		const token = await getToken();
+    const createDoc = async () => {
+        // Create a new document
+        const title = document.querySelector("#doc-title").value;
+        const content = document.querySelector("#doc-text").value;
+        const token = await getToken();
 
-		let res = await axios.post('http://localhost:8080/api/doc/new', {
-			"token": token
-		}).catch(err => console.log(err));
+        let res = await axios
+            .post("http://localhost:8080/api/doc/new", {
+                token: token,
+            })
+            .catch((err) => console.log(err));
 
-		if (res.status == 200)
-		{
-			currentDoc = res.data.id;
-			res = await axios.post(`http://localhost:8080/api/doc/${currentDoc}`, {
-				"token": token,
-				"title": title,
-				"content": content,
-			}).catch(err => console.log(err));
-		}
-	};
+        if (res.status == 200) {
+            currentDoc = res.data.id;
+            res = await axios
+                .post(`http://localhost:8080/api/doc/${currentDoc}`, {
+                    token: token,
+                    title: title,
+                    content: content,
+                })
+                .catch((err) => console.log(err));
+        }
+    };
 
-	const deleteDoc = async () =>
-	{
-		console.log(currentDoc);
-		const token = await getToken();
-		let res = await axios.post(`http://localhost:8080/api/doc/delete/${currentDoc}`, {
-			"token": token
-		}).catch(err => console.log(err));
-	}
+    const deleteDoc = async () => {
+        console.log(currentDoc);
+        const token = await getToken();
+        let res = await axios
+            .post(`http://localhost:8080/api/doc/delete/${currentDoc}`, {
+                token: token,
+            })
+            .catch((err) => console.log(err));
+    };
 
-	return (
-		<div className="flex flex-col justify-center items-center">
-			<h1 className="text-xl font-bold">Dashboard</h1>
-			<p>This is your dashboard</p>
-			<p>There should be something here</p>
-			<button onClick={signOut}>Sign Out</button>
-			<input id="doc-title" style={{ color: "black", padding: 10, marginTop: 10 }} type="text" />
-			<textarea id="doc-text" style={{ color: "black", padding: 10, marginTop: 10 }} name="text" cols="30" rows="10"></textarea>
-			<div style={{display: "flex"}}>
-				<button onClick={createDoc} style={{ color: "black", backgroundColor: "white", padding: 10, borderRadius: 5, margin: 10 }}>Save doc</button>
-				<button onClick={deleteDoc} style={{ color: "black", backgroundColor: "white", padding: 10, borderRadius: 5, margin: 10 }}>Delete doc</button>
-			</div>
-		</div>
-	);
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <h1 className="text-xl font-bold">Dashboard</h1>
+            <p>This is your dashboard</p>
+            <p>There should be something here</p>
+            <button onClick={signOut}>Sign Out</button>
+            <input
+                id="doc-title"
+                style={{ color: "black", padding: 10, marginTop: 10 }}
+                type="text"
+            />
+            <textarea
+                id="doc-text"
+                style={{ color: "black", padding: 10, marginTop: 10 }}
+                name="text"
+                cols="30"
+                rows="10"
+            ></textarea>
+            <div style={{ display: "flex" }}>
+                <button
+                    onClick={createDoc}
+                    style={{
+                        color: "black",
+                        backgroundColor: "white",
+                        padding: 10,
+                        borderRadius: 5,
+                        margin: 10,
+                    }}
+                >
+                    Save doc
+                </button>
+                <button
+                    onClick={deleteDoc}
+                    style={{
+                        color: "black",
+                        backgroundColor: "white",
+                        padding: 10,
+                        borderRadius: 5,
+                        margin: 10,
+                    }}
+                >
+                    Delete doc
+                </button>
+            </div>
+        </div>
+    );
 }
