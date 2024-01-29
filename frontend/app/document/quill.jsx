07286e33dbe62b165dfa9827842a50467f1157f5
DarkEmbers <tsharihar@gmail.com>
2024-01-29 13:11:49 +0400

    Doc design update

--- frontend/app/document/quill.jsx
+++ frontend/app/document/quill.jsx
@@ -7,28 +7,23 @@ export default function Quill()
 	const [value, setValue] = useState("");
 
 	var toolbarOptions = [
-		['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-		['blockquote', 'code-block'],
-	  
-		[{ 'header': 1 }, { 'header': 2 }],               // custom button values
-		[{ 'list': 'ordered'}, { 'list': 'bullet' }],
-		[{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-		[{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-		[{ 'direction': 'rtl' }],                         // text direction
-	  
-		[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-		[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-	  
-		[{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
 		[{ 'font': [] }],
+
+		[{ 'header': [1, 2, 3, 4, 5, 6, false] }, { 'header': 1 }, { 'header': 2 }],
+
+		[{ 'color': [] }, { 'background': [] }], // dropdown with defaults from theme
+
+		['bold', 'italic', 'underline', 'strike', { 'script': 'sub' }, { 'script': 'super' }], // toggled buttons
+		[{ 'list': 'ordered' }, { 'list': 'bullet' }],
+		['code-block'],
+
+		[{ 'indent': '-1' }, { 'indent': '+1' }], // outdent/indent
+		[{ 'direction': 'rtl' }], // text direction
 		[{ 'align': [] }],
-	  
-		['clean'],                                        // remove formatting button
-	  ];
+	];
 
 	const module = {
 		toolbar: toolbarOptions,
-		
 	};
 
 	return <ReactQuill modules={module} theme="snow" value={value} onChange={setValue} />;
