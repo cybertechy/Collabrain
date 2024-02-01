15a56f27d321593a5df265b9d65146863b90ae49
DarkEmbers <tsharihar@gmail.com>
2024-02-02 00:48:43 +0400

    Cleanup doc editor

--- frontend/app/document/page.jsx
+++ frontend/app/document/page.jsx
@@ -1,29 +1,25 @@
 "use client";
 
 const fb = require("_firebase/firebase");
-// const axios = require('axios');
-import Sidebar from "../../components/ui/sidebar/sidebar";
-import Navbar from "../../components/ui/navbar/navbar";
+import Template from "@/components/ui/template/template";
 import { Editor } from "novel";
 
 export default function TextEditor()
 {
 	return (
-		<div className="flex flex-grow overflow-hidden">
-
-			<Sidebar />
-			<div className="flex-col flex-1 overflow-hidden">
-				<Navbar />
-				<div className="flex justify-center bg-gray-200 shadow-md rounded-md overflow-x-hidden overflow-y-auto flex-1">
+		<Template> 
+			<div className="flex flex-grow overflow-hidden">
+				<div className="flex justify-center bg-gray-200 shadow-md rounded-md flex-1">
 					<Editor
 						defaultValue={{
 							type: "doc",
 							content: []
 						}}
 						disableLocalStorage={true}
+						className="w-full overflow-x-hidden overflow-y-auto bg-white"
 					/>
 				</div>
 			</div>
-		</div>
+		</Template>
 	);
 }
