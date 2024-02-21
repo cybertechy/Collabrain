d279a9fc3a12b1d41f6399bd0b679efc8c273581
kirsantol <k.tolstobrov@gmail.com>
2024-02-21 18:31:10 +0400

    Fixed error related to broadcastMessage

--- backend/src/api/helpers/socket.js
+++ backend/src/api/helpers/socket.js
@@ -58,31 +58,40 @@ function init(server)
 
 async function broadcastMessage(data, type = "team")
 {
-	let members = type == "team" ? await fb.getTeamMembers(data.team): await fb.getChatMembers(data.chat) ;
-	let membersList = members;
+    let members = type == "team" ? await fb.getTeamMembers(data.team): await fb.getChatMembers(data.chat) ;
+    let membersList = Array.isArray(members) ? members : [];
 
-	// if their is a sentAt field, convert it to a firebase timestamp
-	let DateBackup = data.sentAt;
-	if (data.sentAt){
-		data.sentAt = fb.admin.firestore.Timestamp.fromDate(new Date(data.sentAt));
-	}
+    // if their is a sentAt field, convert it to a firebase timestamp
+    let DateBackup = data.sentAt;
+    const date = new Date(data.sentAt)
+    if (data.sentAt){
+         if (!isNaN(date.getTime())) {
+            // If sentAt is in date format, creates a timestamp
+            data.sentAt = fb.admin.firestore.Timestamp.fromDate(new Date(data.sentAt));;
+        } else {
+			// Otherwise, converts milliseconds and nanoseconds into a timestamp
+            data.sentAt = fb.admin.firestore.Timestamp.fromDate(new Date(new Date(data.sentAt.seconds * 1000 + 
+                data.sentAt.nanoseconds / 1000, ).toLocaleDateString()));
+        }
+    }
 
-	// Remove the sender
-	const index = membersList.indexOf(data.senderID);
-	if (index > -1) // only splice array when item is found
-		membersList.splice(index, 1); // 2nd parameter means remove one item only
+    // Remove the sender
+    const index = membersList.indexOf(data.senderID);
+    if (index > -1) // only splice array when item is found
+        membersList.splice(index, 1); // 2nd parameter means remove one item only
 
-	// Send to all online members
-	membersList.forEach((member) =>
-	{
-		if (Object.keys(currLinks).includes(member))
-			io.to(currLinks[member]).emit((type == "team") ? "teamMsg" : "directMsg", data);
-	});
+    // Send to all online members
+    membersList.forEach((member) =>
+    {
+        if (Object.keys(currLinks).includes(member))
+            io.to(currLinks[member]).emit((type == "team") ? "teamMsg" : "directMsg", data);
+    });
 
-	// Restore the sentAt field
-	data.sentAt = DateBackup;
-	(type == "team") ? fb.saveTeamMsg(data) : fb.saveDirectMsg(data);
+    // Restore the sentAt field
+    data.sentAt = DateBackup;
+    (type == "team") ? fb.saveTeamMsg(data) : fb.saveDirectMsg(data);
 }
+
 
 module.exports = {
 	init,
