f066e16f72b2ef4151200995b6c8df1653b87301
DarkEmbers <tsharihar@gmail.com>
2024-02-23 19:44:13 +0400

    Added untested doc endpoints

--- backend/src/api/routes/Doc.js
+++ backend/src/api/routes/Doc.js
@@ -1,62 +1,224 @@
 const express = require('express');
 const router = express.Router();
-const db = require("../helpers/firebase");
+const fb = require("../helpers/firebase");
+const uuid = require("uuid");
+const oci = require("../helpers/oracle");
 
-// Doc api endpoints
-// NOTE: Need to be tested
+// Make an enum for user roles
+const roles = {
+	viewer: "viewer",
+	editor: "editor",
+	owner: "owner"
+};
 
-router.post("/", (req, res) =>
+// Create new document
+router.post('/', async (req, res) =>
 {
-	try
-	{
-		db.verifyUser(req.body.token).then(user =>
+	// Make sure all required fields are present
+	if (!req.headers.authorization || !req.body.name)
+		return res.status(400).json({ error: "Missing required data" });
+
+	// verify token
+	let user = await fb.verifyUser(req.headers.authorization.split(" ")[1]); // Get token from header
+	if (!user)
+		return res.status(401).json({ error: "Unauthorized" });
+
+	// Create andn upload new document to oracle cloud
+	let dataId = uuid.v4();
+	const uploadData = await oci.addData("B3", dataId, "application/json", JSON.stringify({}));
+	if (!uploadData.eTag)
+		return res.status(500).json({ error: "Failed to create document" });
+
+	const doc = {
+		name: req.body.name,
+		data: dataId,
+		createdAt: fb.admin.firestore.FieldValue.serverTimestamp(),
+		updatedAt: fb.admin.firestore.FieldValue.serverTimestamp(),
+		access: { [user.uid]: { role: roles.owner } },
+		path: (req.body.path) ? req.body.path : "/"
+	};
+
+	// Add document collection
+	fb.db.collection(`documents`).add(doc)
+		.then((docRef) =>
 		{
-			if (!user)
-				throw new Error("Unauthorized");
+			fb.db.doc(`users/${user.uid}`).update({
+				documents: fb.admin.firestore.FieldValue.arrayUnion(docRef.id)
+			})
+				.then(() => { res.status(200).json({ message: "Document created", id: docRef.id }); })
+				.catch((error) => { res.status(500).json({ error: "Failed to create document" }); });
 
-			db.createDoc(user.uid)
-				.then((ref) => { res.json({ message: "Document created", id: ref.id }); })
-				.catch((err) => { res.status(500).json({ error: err }); });
-
-		});
-	}
-	catch (error) { res.status(401).json({ error: error }); }
+		}).catch((error) => { res.status(500).json({ error: "Failed to create document" }); });
 });
 
-router.post("/:ref", (req, res) =>
+// Get all documents of user
+router.get('/all', async (req, res) =>
 {
-	try
-	{
-		db.verifyUser(req.body.token).then(user =>
+	// Make sure all required fields are present
+	if (!req.headers.authorization)
+		return res.status(400).json({ error: "Missing required data" });
+
+	// verify token
+	let user = await fb.verifyUser(req.headers.authorization.split(" ")[1]); // Get token from header
+	if (!user)
+		return res.status(401).json({ error: "Unauthorized" });
+
+	// Get all documents of user
+	fb.db.collection("documents").where(`access.${user.uid}.role`, "!=", "null").get()
+		.then((snapshot) =>
 		{
-			if (!user)
-				throw new Error("Unauthorized");
+			let documents = [];
+			snapshot.forEach((doc) => { documents.push({ id: doc.id, ...doc.data() }); });
+			res.status(200).json(documents);
 
-			db.updateDoc(user.uid, req.params.ref, req.body.title, req.body.content)
-				.then(() => { res.json({ message: "Document updated" }); })
-				.catch((err) => { res.status(500).json({ error: err}); });
-
-		});
-	}
-	catch (err) { res.status(401).json({ error: err }); }
+		}).catch((error) => { res.status(500).json({ error: "Failed to get documents" }); });
 });
 
-router.delete("/:ref", (req, res) =>
+// Get document by id
+router.get('/:id', async (req, res) =>
 {
-	try
-	{
-		db.verifyUser(req.body.token).then(user =>
+	// Make sure all required fields are present
+	if (!req.headers.authorization)
+		return res.status(400).json({ error: "Missing required data" });
+
+	// verify token
+	let user = await fb.verifyUser(req.headers.authorization.split(" ")[1]); // Get token from header
+	if (!user)
+		return res.status(401).json({ error: "Unauthorized" });
+
+	// Check if user has access to document
+	fb.db.doc(`users/${user.uid}`).get()
+		.then((doc) =>
 		{
-			if (!user)
-				throw new Error("Unauthorized");
+			if (!doc.data().documents.includes(req.params.id))
+				return res.status(401).json({ error: "Unauthorized" });
 
-			db.removeDoc(user.uid, req.params.ref)
-				.then(() => { res.json({ message: "Document deleted" }); })
-				.catch((error) => { res.status(500).json({ error: "Failed to delete doc" }); });
+			// Get document
+			fb.db.doc(`documents/${req.params.id}`).get()
+				.then((doc) => { res.status(200).json({ id: doc.id, ...doc.data() }); })
+				.catch((error) => { res.status(500).json({ error: "Failed to get document" }); });
+		})
+		.catch((error) => { res.status(500).json({ error: "Failed to get document" }); });
+});
 
-		});
-	}
-	catch (error) { res.status(401).json({ message: error }); }
+// Delete document
+router.delete('/:id', async (req, res) =>
+{
+	// Make sure all required fields are present
+	if (!req.headers.authorization)
+		return res.status(400).json({ error: "Missing required data" });
+
+	// verify token
+	let user = await fb.verifyUser(req.headers.authorization.split(" ")[1]); // Get token from header
+	if (!user)
+		return res.status(401).json({ error: "Unauthorized" });
+
+	// Check if user has is owner of document
+	let docRef = fb.db.doc(`documents/${req.params.id}`);
+	docRef.get()
+		.then((doc) =>
+		{
+			if (doc.data().access[user.uid].role !== roles.owner)
+				return res.status(401).json({ error: "Unauthorized" });
+
+			// Get all users with access to document
+			let users = Object.keys(doc.data().access);
+			// Delete document from all users
+			fb.db.collection(`users`).where("uid", "in", users).get()
+				.then((snapshot) =>
+				{
+					snapshot.forEach((userDoc) =>
+					{
+						userDoc.ref.update({
+							documents: fb.admin.firestore.FieldValue.arrayRemove(req.params.id)
+						});
+					});
+				});
+
+			// Remove doc from oracle cloud
+			oci.deleteData("B3", doc.data().data);
+			// Delete document
+			docRef.delete()
+				.then(() => { res.status(200).json({ message: "Document deleted" }); })
+				.catch((error) => { throw error; });
+		})
+		.catch((error) => { res.status(500).json({ error: "Failed to delete document" }); });
+});
+
+// Share document
+router.post('/:id/share/:user', async (req, res) =>
+{
+	// Make sure all required fields are present
+	if (!req.headers.authorization || !req.body.role)
+		return res.status(400).json({ error: "Missing required data" });
+
+	// verify token
+	let user = await fb.verifyUser(req.headers.authorization.split(" ")[1]); // Get token from header
+	if (!user)
+		return res.status(401).json({ error: "Unauthorized" });
+
+	let docRef = fb.db.doc(`documents/${req.params.id}`);
+	docRef.get()
+		.then((doc) =>
+		{
+			// Check if user is owner of document
+			if (doc.data().access[user.uid].role !== roles.owner)
+				return res.status(401).json({ error: "Unauthorized" });
+
+			// Add document to user
+			fb.db.doc(`users/${req.params.user}`).update({
+				documents: fb.admin.firestore.FieldValue.arrayUnion(req.params.id)
+			})
+				.then(() =>
+				{
+					// Add user to document
+					docRef.update({
+						[`access.${req.params.user}`]: { role: req.body.role }
+					})
+						.then(() => { res.status(200).json({ message: "Document shared" }); })
+						.catch((error) => { throw error; });
+				})
+				.catch((error) => { throw error; });
+		})
+		.catch((error) => { res.status(500).json({ error: "Failed to share document" }); });
+});
+
+// Remove user from document
+router.delete('/:id/share/:user', async (req, res) =>
+{
+	// Make sure all required fields are present
+	if (!req.headers.authorization)
+		return res.status(400).json({ error: "Missing required data" });
+
+	// verify token
+	let user = await fb.verifyUser(req.headers.authorization.split(" ")[1]); // Get token from header
+	if (!user)
+		return res.status(401).json({ error: "Unauthorized" });
+
+	let docRef = fb.db.doc(`documents/${req.params.id}`);
+	docRef.get()
+		.then((doc) =>
+		{
+			// Check if user is owner of document
+			if (doc.data().access[user.uid].role !== roles.owner)
+				return res.status(401).json({ error: "Unauthorized" });
+
+			// Remove document from user
+			fb.db.doc(`users/${req.params.user}`).update({
+				documents: fb.admin.firestore.FieldValue.arrayRemove(req.params.id)
+			})
+				.then(() =>
+				{
+					// Remove user from document
+					docRef.update({
+						[`access.${req.params.user}`]: fb.admin.firestore.FieldValue.delete()
+					})
+						.then(() => { res.status(200).json({ message: "Document unshared" }); })
+						.catch((error) => { throw error; });
+				})
+				.catch((error) => { throw error; });
+		})
+		.catch((error) => { res.status(500).json({ error: "Failed to unshare document" }); });
 });
 
 module.exports = router;	
