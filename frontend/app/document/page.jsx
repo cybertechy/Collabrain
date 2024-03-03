26a74a92e831d14fba0b51e7498eb58bce8bc71f
DarkEmbers <tsharihar@gmail.com>
2024-03-03 18:11:28 +0400

    Add animations, Add share button, bug fixes

--- frontend/app/document/page.jsx
+++ frontend/app/document/page.jsx
@@ -1,31 +1,43 @@
 "use client";
 
 import dynamic from "next/dynamic";
-const { useRouter } = require("next/navigation");
-import Template from "_components/ui/template/template";
-const Quill = dynamic(() => import("./quill"), { ssr: false });
-import axios from 'axios';
-const socket = require("_socket/socket");
-const fb = require("_firebase/firebase");
-import "./quillStyle.css";
 import { useRef, useState, useEffect } from "react";
-import { useSearchParams } from 'next/navigation';
+const { useRouter, useSearchParams } = require("next/navigation");
+
+import axios from 'axios';
+import Lottie from "lottie-react";
+
+const fb = require("_firebase/firebase");
+const socket = require("_socket/socket");
+import Template from "_components/ui/template/template";
 import FileToolbar from "_components/ui/FileToolbar/fileToolbar";
-import Comments from "./comments";
+const Quill = dynamic(() => import("./quill"), { ssr: false });
+import "./quillStyle.css";
+
+import SearchingJSON from "../../public/assets/json/Searching.json";
+import LoadingJSON from "../../public/assets/json/Loading.json";
+import ErrorJSON from "../../public/assets/json/Error.json";
+import WorkingJSON from "../../public/assets/json/Working.json";
 
 export default function Editor()
 {
+	const router = useRouter();
 	const [user, loading] = fb.useAuthState();
 	const searchParams = useSearchParams();
-	const [userData, setUserData] = useState({});
+
+	const [userData, setUserData] = useState();
+	const [fileData, setFileData] = useState();
+	const [ociID, setOciID] = useState("");
+	const [loadFailed, setLoadFailed] = useState(null);
+
+	const quillRef = useRef(null);
 	const [value, setValue] = useState("Loading...");
 	const [isDisabled, setIsDisabled] = useState(true);
-	const [ociID, setOciID] = useState("");
+
 	const [docName, setDocName] = useState("");
 	const [showCommentButton, setShowCommentButton] = useState(false);
-	const quillRef = useRef(null);
-	const router = useRouter();
 
+	// Redirect to dashboard if no document is specified
 	if (searchParams.get('id') == null)
 	{
 		// No document selected
@@ -33,6 +45,7 @@ export default function Editor()
 		router.push("/dashboard");
 	}
 
+	// Generate random color for cursor
 	const randomColor = () =>
 	{
 		const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
@@ -42,6 +55,7 @@ export default function Editor()
 		return `hsl(${h},${s}%,${l}%)`;
 	};
 
+	// Setup socket and event listeners
 	let sockCli = useRef(null);
 	useEffect(() =>
 	{
@@ -83,7 +97,7 @@ export default function Editor()
 	// Load document
 	useEffect(() =>
 	{
-		if (!user || !sockCli.current)
+		if (!user || !userData || !sockCli.current)
 			return;
 
 		const id = searchParams.get('id');
@@ -94,44 +108,86 @@ export default function Editor()
 				headers: {
 					"Authorization": `Bearer ${token}`
 				}
-			}).then(res =>
-			{
-				setDocName(res.data.name);
-				setOciID(res.data.data);
-				setValue(JSON.parse(res.data.contents));
+			})
+				.then(res =>
+				{
+					setFileData(res.data);
+					setDocName(res.data.name);
+					setOciID(res.data.data);
+					setValue(JSON.parse(res.data.contents));
 
-				if (res.data.access[userData.uid] != "viewer")
-					setIsDisabled(false);
-			});
+					if (res.data.access[userData.uid] != "view")
+						setIsDisabled(false);
+				})
+				.catch(err =>
+				{
+					console.log(err.response.data.error);
+					setLoadFailed(err.response.data.error);
+				});
 		});
 
 		sockCli.current.emit('join-doc', id);
 		return () => sockCli.current.emit('leave-doc', id);
-	}, [user]);
+	}, [user, userData]);
 
-	if (loading)
+	// Signing in
+	if (loading || !user || !userData)
 	{
-		// Not signed in
 		return (
 			<Template>
-				<div className="bg-[#F3F3F3] h-full">
-					<div className="editor-container bg-[#F3F3F3]">
-						<div className="text-center text-3xl mt-20">Loading...</div>
-					</div>
+				<div className="bg-[#F3F3F3] w-screen h-full flex flex-col justify-center items-center text-basicallydark">
+					<Lottie animationData={LoadingJSON} style={{ width: 300, height: 300 }} />
+					<h1 className="text-2xl font-bold text-primary px-10">Loading...</h1>
 				</div>
 			</Template>
 		);
 	}
 
+	if (loadFailed)
+	{
+		return (
+			<Template>
+				<div className="bg-[#F3F3F3] w-screen h-full flex flex-col justify-center items-center text-basicallydark">
+					<Lottie animationData={ErrorJSON} style={{ width: 300, height: 300 }} />
+					<h1 className="text-2xl font-bold text-primary px-10 text-center"> {loadFailed} </h1>
+				</div>
+			</Template>
+		);
+	}
+
+	if (!fileData || !ociID || !docName)
+	{
+		return (
+			<Template>
+				<div className="bg-[#F3F3F3] w-screen h-full flex flex-col justify-center items-center text-basicallydark">
+					<Lottie animationData={WorkingJSON} style={{ width: 300, height: 300 }} />
+					<h1 className="text-2xl font-bold text-primary px-10 text-center"> Working to load the Doc ... </h1>
+				</div>
+			</Template>
+		);
+
+	}
+
 	return (
 		<Template>
-			<FileToolbar name={docName} />
-			<div className="editor-container bg-[#F3F3F3] overflow-auto">
-				<Quill socket={sockCli} user={userData} quillRef={quillRef}
-					value={value} setValue={setValue} isDisabled={isDisabled}
-					docID={searchParams.get('id')} ociID={ociID}
-					setShowCommentButton={setShowCommentButton} />
-				{/* <Comments /> */}
+			{user && fileData &&
+				<FileToolbar userID={user.uid} name={docName} commentsEnabled={true} showCommentButton={showCommentButton}
+					fileType="doc" fileID={searchParams.get('id')} fileData={fileData} />
+			}
+
+			<div className="relative flex overflow-y-auto h-fit">
+				<div className="editor-container flex-1 bg-[#F3F3F3] h-fit">
+					<Quill socket={sockCli} user={userData} quillRef={quillRef}
+						value={value} setValue={setValue} isDisabled={isDisabled}
+						docID={searchParams.get('id')} ociID={ociID}
+						setShowCommentButton={setShowCommentButton} />
+				</div>
+
+				{/* {showComments &&
+					// <div className="w-1/4 bg-[#F3F3F3] shadow-[0_0_5px_0_rgba(0,0,0,0.5)] z-[1]">
+
+					// </div>
+				} */}
 			</div>
 		</Template>
 	);
--- frontend/components/ui/FileToolbar/fileToolbar.jsx
+++ frontend/components/ui/FileToolbar/fileToolbar.jsx
@@ -1,16 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CommentIcon from '@mui/icons-material/Comment';
+import AddCommentIcon from '@mui/icons-material/AddComment';
+import { RefreshCcw, FilePenLine, HelpCircle, Type } from 'lucide-react';
+import ShareComponent from "_components/ui/share";
+const axios = require('axios');
+const fb = require("_firebase/firebase");
+import { ToastContainer, toast } from "react-toastify";
 
+
+/**
+ * @param props.userID - The user's uid
+ * @param props.name - The name of the file
+ * @param props.fileType - The type of file (i.e. "doc"/"map")
+ * @param props.fileData - The data of the file (i.e. as returned by the API)
+ * @param props.fileID - The name of the file
+ * @param props.commentsEnabled - Whether comments are enabled, defaults to false
+ * @param props.showCommentButton - Whether to show the comment button, only needed if comments are enabled
+ */
 export default function FileToolbar(props)
 {
+	const [Share, setShare] = useState(false);
+	const [token, setToken] = useState("");
+
+	// Function to update file data for sharing
+	const updateFileData = async (newData) =>
+	{
+		let res;
+		// Different API calls for different file types due to inconsistencies
+		if (props.fileType === "doc")
+		{
+			res = await axios.patch(`http://localhost:8080/api/${props.fileType}s/${props.fileID}`, newData, {
+				headers: { "Authorization": "Bearer " + token }
+			});
+		}
+		else if (props.fileType === "map")
+		{
+			res = await axios.put(`http://localhost:8080/api/${props.fileType}s/${props.fileID}`, newData, {
+				headers: { "Authorization": "Bearer " + token }
+			});
+		}
+
+		return res;
+	};
+
+	useEffect(() =>
+	{
+		fb.getToken().then((token) => setToken(token));
+	}, []);
+
 	return (
-		<div className="flex justify-between items-center p-4 bg-[#f3f3f3] border-b border-gray-300">
+		<div className="flex justify-between items-center p-4 bg-[#f3f3f3] border-b border-gray-300 z-[2]">
 			{/* Document Name (leftmost element) */}
 			<div className="text-lg font-bold">{props.name}</div>
 
-			{/* Share Button on the right */}
-			<button className="bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded text-white focus:outline-none">
-				Share
-			</button>
+			{/* Buttons container (right side) */}
+			<div className="flex space-x-2">
+				{/* Add comment button */}
+				{props.commentsEnabled && props.showCommentButton && (
+					<button className="bg-blue-500 hover:bg-blue-600 px-2 py-1 rounded text-white focus:outline-none"> {/* Reduced padding */}
+						<AddCommentIcon />
+					</button>
+				)}
+
+				{/* Show comments button */}
+				{props.commentsEnabled && (
+					<button className="bg-blue-500 hover:bg-blue-600 px-2 py-1 rounded text-white focus:outline-none">
+						<CommentIcon />
+					</button>
+				)}
+
+				<ToastContainer />
+
+				{/* Share button */}
+				<button id="share" onClick={() => setShare(Share => !Share)} className="bg-blue-500 hover:bg-blue-600 px-2 py-1 rounded text-white focus:outline-none">Share</button>
+				{Share && <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 bg-basicallylight rounded-md shadow-[0_0_10px_0_rgba(0,0,0,0.2)] p-2 flex flex-col gap-2 border border-primary">
+					<ShareComponent getdata={props.fileData} updatecontent={updateFileData}
+						contentMapName={props.name} setShare={setShare}
+						sData={props.fileData.access} isOwner={props.fileData.access[props.userID].role == "owner"} />
+				</div>}
+
+				{/* Save button */}
+				{/* {(IntialData?.userAccess === "edit" || IntialData?.userAccess === "owner") && <div id="save" className="flex items-center gap-2 bg-basicallylight text-primary rounded-md  px-2 py-1 ">
+					<p className={`rounded-t-lg `}> <RefreshCcw width={20} height={20} className={`${!isSaved && "animate-spin"}`} />  </p>
+					<p className="lg:block hidden  "> {isSaved ? "Saved" : "Saving..."}</p>
+				</div>} */}
+			</div>
 		</div>
 	);
 };
