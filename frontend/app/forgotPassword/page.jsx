0b94db0b986cc0461aa729406ab3ef3f33aa3d9f
Sarah Syeda <98742887+Sarah2211@users.noreply.github.com>
2024-03-17 15:22:22 +0400

    Updated forgot password ui

--- frontend/app/forgotPassword/page.jsx
+++ frontend/app/forgotPassword/page.jsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify'
 import EmailInputField from "../../components/ui/input/emailinput2";
 import Button from "../../components/ui/button/button";
 import fb from "_firebase/firebase";
+
 
 const ForgotPassword = () => {
 
@@ -22,15 +23,55 @@ const ForgotPassword = () => {
     }
   };
 
+  const [backgroundLoaded, setBackgroundLoaded] = useState(false);
+  useEffect(() => {
+    // Preload the background image
+    const img = new Image();
+    img.src = '/assets/images/background.jpg'; // Adjust the path to your background image
+    img.onload = () => {
+        setBackgroundLoaded(true);
+        document.body.classList.add('custom-background');
+    };
+
+    // Remove the custom background class when the component unmounts
+    return () =>
+    {
+        document.body.classList.remove('custom-background');
+    };
+}, []);
+
+if (!backgroundLoaded)
+{
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="loader"></div>
+        </div>
+    );
+}
+
+
+
   return (
+
     <div className="max-sm:min-h-screen flex items-center justify-center min-h-screen">
+
+
       <ToastContainer />
+
+
       <div className="flex flex-col items-center justify-center">
+      <img
+                        className="w-40"
+                        src="./assets/images/logo_whitebackground.png"
+                        alt="Logo"
+                    />
+        
+        <div className="bg-basicallylight drop-shadow-lg sm:p-10 rounded-2xl sm:mt-4 items-center justify-center">
         <h1 className="text-2xl text-primary font-sans mb-6 text-center">
           Forgot Your Password?
         </h1>
-        <div className="bg-basicallylight drop-shadow-lg sm:p-10 rounded-2xl sm:mt-4 items-center justify-center">
           <div className="flex w-full max-sm:pr-4 items-center justify-center">
+
             <EmailInputField
               name="email"
               value={email}
@@ -48,7 +89,7 @@ const ForgotPassword = () => {
               className="mt-4 mr-2"
             />
           </div>
-          <div className="flex justify-center">
+          <div className="flex justify-center text-primary">
             <a href="/" className="underline">
               Back to login
             </a>
