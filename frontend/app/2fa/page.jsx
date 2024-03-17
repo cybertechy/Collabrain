7a9b779cd177282c9ee3838d1f12ec17d5acf840
Sarah Syeda <98742887+Sarah2211@users.noreply.github.com>
2024-03-17 15:02:11 +0400

    Update page.jsx for 2fa

--- frontend/app/2fa/page.jsx
+++ frontend/app/2fa/page.jsx
@@ -1,42 +1,39 @@
-// OTPVerificationPage.js
 "use client";
 import React, { useState } from 'react';
-const { useRouter } = require("next/navigation");
+const { useRouter } = require("next/navigation"); 
 import { ToastContainer, toast } from 'react-toastify';
 import Button from "../../components/ui/button/button";
 import InputField from '../../components/ui/input/input';
 import axios from 'axios';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Link from 'next/link';
 const fb = require("_firebase/firebase");
 
-const handleSendOTP = async () => {
-    try {
-        const token = await fb.getToken();
-      const response = await axios.post('http://localhost:8080/api/2FA/sendCode', null, {
-        headers: { Authorization: `Bearer ${token}` }, // Include authorization token if required
-      });
-      // Handle the response as needed
-      console.log(response.data.message); // Assuming your backend sends a message
-      // Optionally, you can show a success message to the user
-    } catch (error) {
-      // Handle errors, such as network errors or API response errors
-      console.error('Error sending OTP:', error.message);
-      // Optionally, show an error message to the user
-    }
-  };
-   
-
- 
 const OTPVerificationPage = () => {
   const router = useRouter();
   const [otp, setOTP] = useState('');
- 
+
+  const handleSendOTP = async () => {
+    try {
+      const token = await fb.getToken();
+      const response = await axios.post('http://localhost:8080/api/2FA/sendCode', null, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log(response.data.message);
+      toast.success('OTP sent successfully');
+    } catch (error) {
+      console.error('Error sending OTP:', error.message);
+      toast.error('Error sending OTP');
+    }
+  };
+
   const handleOTPVerification = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post('http://localhost:8080/api/2FA/verifyCode', { code: otp });
       if (response.status === 200) {
         toast.success('OTP verified successfully');
-        // Redirect to dashboard or other page on successful OTP verification
+ 
         router.push('/dashboard');
       } else {
         toast.error('Failed to verify OTP');
@@ -46,24 +43,35 @@ const OTPVerificationPage = () => {
       toast.error('Error verifying OTP');
     }
   };
- 
-  return (
-<div className="max-w-md mx-auto mt-10">
-<ToastContainer />
 
-<button onClick={handleSendOTP} className="text-center inline-flex items-center border border-primary text-primary hover:bg-primary hover:text-basicallylight px-7 py-3 ">Send OTP</button> 
-<h1 className="text-2xl font-bold mb-4">Enter OTP to Verify</h1>
-<form onSubmit={handleOTPVerification}>
-<InputField
-          type="text"
-          placeholder="Enter OTP"
-          value={otp}
-          onChange={(e) => setOTP(e.target.value)}
-        />
-<Button type="submit" text="Verify OTP" color="primary" className="mt-4" />
-</form>
-</div>
+  return (
+    <div className="max-sm:min-w-full md:w-3/4 lg:w-1/2 mx-auto max-w-md">
+      <ToastContainer />
+      <div className="justify-center items-center flex flex-col min-h-screen">
+        
+        <form onSubmit={handleOTPVerification} className="bg-basicallylight drop-shadow-lg flex flex-col justify-center items-center px-16 py-10 rounded-2xl">
+        <Link href="/">
+          
+            <ArrowBackIcon className="text-primary cursor-pointer absolute left-4 top-4" />
+          
+        </Link>
+        <h1 className="text-2xl text-primary font-bold mb-4">Enter OTP to Verify</h1>
+          <InputField
+            type="text"
+            placeholder="Enter OTP"
+            value={otp}
+            onChange={(e) => setOTP(e.target.value)}
+            className="mt-4"
+          />
+          <Button type="submit" text="Verify OTP" color="primary" className="mt-4" />
+          <p>Click here to receive OTP</p>
+          <button onClick={handleSendOTP} className="underline underline-offset-1 text-primary">Send OTP</button>
+
+        </form>
+      </div>
+    </div>
   );
 };
- 
+
 export default OTPVerificationPage;
+
