2b1c135fcc6620c846f528e9178d5875f1c30d14
Cybertechy <siddhtailor96@gmail.com>
2024-03-23 22:54:06 +0400

    Fix leaderboard

--- frontend/app/(signedIn)/leaderboard/page.jsx
+++ frontend/app/(signedIn)/leaderboard/page.jsx
@@ -7,9 +7,9 @@ import { useAuthState } from "@/app/_firebase/firebase";
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import Template from "@/components/ui/template/template";
 import GroupsIcon from '@mui/icons-material/Groups';
- 
+
 const SERVERLOCATION = process.env.NEXT_PUBLIC_SERVER_LOCATION;
- 
+
 const Leaderboard = () => {
   const [teamData, setTeamData] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
@@ -17,7 +17,7 @@ const Leaderboard = () => {
   const [user, loading] = useAuthState();
   const [clickedRow, setClickedRow] = useState(null);
   const [hoveredRow, setHoveredRow] = useState(null);
- 
+
   const fetchTeams = async () => {
     try {
       const token = await fb.getToken();
@@ -29,7 +29,7 @@ const Leaderboard = () => {
       console.error(error);
     }
   };
- 
+
   useEffect(() => {
     const fetchJoinedTeams = async () => {
       try {
@@ -37,25 +37,25 @@ const Leaderboard = () => {
         const response = await axios.get(`${SERVERLOCATION}/api/teams?names=true`, {
           headers: { Authorization: `Bearer ${token}` },
         });
-        console.log("Joined Team Data: ",response.data);
+        console.log("Joined Team Data: ", response.data);
         setJoinedTeams(response.data);
       } catch (error) {
         console.error(error);
       }
     };
- 
+
     fetchJoinedTeams();
     fetchTeams();
   }, [user]);
- 
+
   const handleRowClick = (teamName) => {
     setClickedRow(clickedRow === teamName ? null : teamName);
   };
- 
+
   const handleSearchChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
- 
+
     if (query === '') {
       fetchTeams(); // Reset the search
     } else {
@@ -65,22 +65,22 @@ const Leaderboard = () => {
       setTeamData(filteredData);
     }
   };
- 
+
   const handleSearchKeyDown = (event) => {
     if (event.key === 'Backspace' && searchQuery === '') {
       fetchTeams(); // Reset the search
     }
   };
- 
-  const handleJoinTeam = async (teamId) => {
+
+  const handleJoinTeam = async (teamId,teamName) => {
     try {
       const token = await fb.getToken();
       const response = await axios.post(`${SERVERLOCATION}/api/teams/${teamId}/users?invite=true`, null, {
         headers: { Authorization: `Bearer ${token}` },
       });
- 
+
       if (response.status === 200) {
-        setJoinedTeams([...joinedTeams, teamId]);
+        setJoinedTeams([...joinedTeams, teamName]);
         console.log('Team joined successfully');
       } else {
         console.error('Error joining team');
@@ -89,10 +89,10 @@ const Leaderboard = () => {
       console.error('Error joining team:', error);
     }
   };
- 
+
   return (
     <>
-      <div className="container mx-auto px-10 pt-8">
+      <div className="container mx-auto px-4 lg:px-10 pt-8">
         <p className="text-3xl font-md text-start text-primary bg-white mb-4">Discover Teams</p>
         <div className="mt-4 flex justify-between items-center">
           <p className="text-xl font-bold text-primary">Joined Teams</p>
@@ -109,7 +109,7 @@ const Leaderboard = () => {
         ) : (
           <p className='mt-3 mb-5 text-gray-400'>No teams joined yet.</p>
         )}
- 
+
         <div className="border-gray-300 inner-shadow border-2 bg-aliceBlue rounded-md drop-shadow-md overflow-visible">
           {/* Search bar */}
           <div className="p-4 flex justify-between">
@@ -123,22 +123,21 @@ const Leaderboard = () => {
               className="p-2 border border-gray-300 rounded-lg w-full"
             />
           </div>
- 
+
           {/* Header row for team data */}
           <div className="flex text-start justify-start bg-primary font-bold px-4 py-2">
-            <span className="text-lg text-white w-1/4">Rank<EmojiEventsIcon className='pl-2 text-3xl'/></span>
+            <span className="text-lg text-white w-1/4">Rank<EmojiEventsIcon className='pl-2 text-3xl' /></span>
             <span className="text-lg text-white w-1/4">Name</span>
             <span className="text-lg text-white w-1/4">Score</span>
             <span className="text-lg text-white w-1/4">Actions</span>
           </div>
- 
+
           <div className="flex flex-col overflow-visible">
             {teamData.map((team, index) => (
               <div
                 key={index}
-                className={`h-16 flex items-center justify-start border-b border-gray-200 py-2 px-4 cursor-pointer relative transform transition-all duration-300 ${
-                  clickedRow === team.name ? 'scale-100 shadow-xl bg-secondary' : 'scale-100', 'border-none', 'rounded-md'
-                }`}
+                className={`h-16 flex items-center justify-start border-b border-gray-200 py-2 px-4 cursor-pointer relative transform transition-all duration-300 ${clickedRow === team.name ? 'scale-100 shadow-xl bg-secondary' : 'scale-100', 'border-none', 'rounded-md'
+                  }`}
                 onClick={() => handleRowClick(team.name)}
                 onMouseEnter={() => setHoveredRow(team.name)}
                 onMouseLeave={() => setHoveredRow(null)}
@@ -148,9 +147,9 @@ const Leaderboard = () => {
                   transform: clickedRow === team.name ? 'scale(1)' : 'scale(1)',
                 }}
               >
-                <span className="text-lg w-1/4 text-primary "># {team.rank}</span>
-                <span className="text-lg w-1/4 text-start text-primary">{team.name}</span>
-                <span className="text-lg w-1/4 text-primary">{team.score}</span>
+                <span className="text-lg w-1/4 pl-2 text-primary "># {team.rank}</span>
+                <span className="text-lg w-1/4 text-start text-primary overflow-hidden">{team.name}</span>
+                <span className="text-lg text-center w-1/4 text-primary">{team.score}</span>
                 {joinedTeams.includes(team.name) ? (
                   <button
                     className=" text-green-500 px-4 py-2 rounded-lg ml-2 cursor-default"
@@ -160,8 +159,8 @@ const Leaderboard = () => {
                   </button>
                 ) : (
                   <button
-                    onClick={() => handleJoinTeam(team.name)}
-                    className="bg-green-500 text-white px-4 py-2 rounded-lg ml-2"
+                    onClick={() => handleJoinTeam(team.id,team.name)}
+                    className="bg-green-500 text-white px-2 lg:px-4 py-1 lg:py-2 rounded-lg ml-2"
                   >
                     Join Team
                   </button>
@@ -174,5 +173,5 @@ const Leaderboard = () => {
     </>
   );
 };
- 
+
 export default Leaderboard;
