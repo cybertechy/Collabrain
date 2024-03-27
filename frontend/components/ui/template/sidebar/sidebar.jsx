d63071ecabf83fd1cc8be87eff41de8dc979f5e1
Cybertechy <siddhtailor96@gmail.com>
2024-03-27 18:39:52 +0400

    remove localhost occurence

--- frontend/components/ui/template/sidebar/sidebar.jsx
+++ frontend/components/ui/template/sidebar/sidebar.jsx
@@ -26,6 +26,8 @@ import { useTTS } from "@/app/utils/tts/TTSContext";
 import "@/app/utils/i18n"
 import { useTranslation } from 'next-i18next';
 
+const SERVERLOCATION = process.env.NEXT_PUBLIC_SERVER_LOCATION;
+
 const Sidebar = ({ teams = {}, isOpen, toggleSidebar }) => {
     
     const { t } = useTranslation('sidebar');
@@ -72,7 +74,7 @@ const Sidebar = ({ teams = {}, isOpen, toggleSidebar }) => {
             try {
                 // Make a GET request to retrieve user's team IDs
                 const token = await fb.getToken();
-                const response = await axios.get('http://localhost:8080/api/teams', {
+                const response = await axios.get(SERVERLOCATION+'/api/teams', {
                     headers: {
                         Authorization: `Bearer ${token}`, // Replace with the actual auth token
                     },
@@ -84,7 +86,7 @@ const Sidebar = ({ teams = {}, isOpen, toggleSidebar }) => {
     
                     // Create an array of promises to fetch team information
                     const teamPromises = teamIds.map(async (teamId) => {
-                        const teamResponse = await axios.get(`http://localhost:8080/api/teams/${teamId}`, {
+                        const teamResponse = await axios.get(`${SERVERLOCATION}/api/teams/${teamId}`, {
                             headers: {
                                 Authorization: `Bearer ${token}`, // Replace with the actual auth token
                             },
