b3d6bf47a575919bf30ce32721e42dfa5884c94d
johanjohnthomas <johanjohnthomas@outlook.com>
2024-05-21 00:24:04 +0400

    fixed landing page responsiveness

--- frontend/components/ui/landing/Navbar.jsx
+++ frontend/components/ui/landing/Navbar.jsx
@@ -10,26 +10,26 @@ const Navbar = () => {
   const [user, loading] = fb.useAuthState();
   
   return (
-    <div className='mx-auto  '>
+    <div className='flex mx-36'> {/* Changed from flex-col to flex for horizontal layout */}
         
-        <div className="container flex flex-col lg:flex-row py-2 justify-between items-center overflow-x-hidden">
+        <div className=" flex flex-col lg:flex-row py-2 justify-between items-center w-full">
             
             <Image src={logo}
+            className="sm:h-40 sm:w-40 h-40 w-40"
             alt="logo" 
             height={90} 
             width={130} />
             
-           
             <div className="flex items-center gap-4">
-                <p className='text-base font-medium cursor-pointer'>Contact Us</p>
-             
-                {!user && <p onClick={()=>router.push("/register")} className='text-base font-medium cursor-pointer'>Sign Up</p>}
-                {!user && <p onClick={()=>router.push("/login")} className='text-base capitalize text-white bg-primary py-2 px-4 rounded-lg font-semibold border border-none cursor-pointer btn btn-sm'>Log In</p>}
-                {user && <p onClick={()=>router.push("/dashboard")} className='text-base capitalize text-white bg-primary py-2 px-4 rounded-lg font-semibold border border-none cursor-pointer btn btn-sm'>Dashboard</p>}
+                <p className='text-base font-medium cursor-pointer' onClick={() => router.push("/contact")}>Contact Us</p>
+                
+                {!user && <p onClick={() => router.push("/register")} className='text-base font-medium cursor-pointer'>Sign Up</p>}
+                {!user && <p onClick={() => router.push("/login")} className='text-base capitalize text-white bg-primary py-2 px-4 rounded-lg font-semibold cursor-pointer'>Log In</p>}
+                {user && <p onClick={() => router.push("/dashboard")} className='text-base capitalize text-white bg-primary py-2 px-4 rounded-lg font-semibold cursor-pointer'>Dashboard</p>}
             </div>
         </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
