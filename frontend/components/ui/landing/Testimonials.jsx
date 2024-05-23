e0c57fe5f65b1fe365529ac214376a10c38a5b2f
Cybertechy <siddhtailor96@gmail.com>
2024-05-23 09:58:46 +0400

    Fixed testimonials padding

--- frontend/components/ui/landing/Testimonials.jsx
+++ frontend/components/ui/landing/Testimonials.jsx
@@ -4,11 +4,9 @@ import logo from '@/public/assets/images/logo_whitebackground.png'
 
 const Testimonials = () => {
   return (
-    <div className='container mx-auto flex flex-col justify-center items-center bg-gray-50 py-10'>
-              
-
+    <div className='container mx-auto flex flex-col justify-center items-center bg-gray-50 py-10 px-4 '>
         <Image className='' src={logo} alt="collabrain logo" />
-        <p className='text-3xl font-semibold'><br />Revolutionize Education: Partner with Collabrain!</p>
+        <p className='text-3xl font-semibold text-center'><br />Revolutionize Education: Partner with Collabrain!</p>
         <p className='text-1xl text-center '><br />Collabrain is currently used by over 400 universities and educational institutes worldwide. <br />
         Contact us and become a part of the digital learning wave 🏄🏻‍♂️<br/></p>
         <br/>
